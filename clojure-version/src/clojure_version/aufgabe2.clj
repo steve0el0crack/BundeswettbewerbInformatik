dfda6de364e9a9c04cc2406507c234d27c20d99a
Esteban Castillo <escaob1@gmail.com>
2020-11-10 17:45:07 +0100

    Die Grunddatenstruktur (Hashmap) ist schon definiert. Man braucht nur die Logic-engine um in dieser Struktur mitendrin zu suchen

--- clojure-version/src/clojure_version/aufgabe2.clj
+++ clojure-version/src/clojure_version/aufgabe2.clj
@@ -1,6 +1,7 @@
 (ns clojure-version.aufgabe2  
   (:require [clojure.java.io :as io]
             [clojure.core.logic :as logic]
+            [clojure.core.logic.fd :as fd]
             [clojure-version.aufgabe1 :as aufgabe1]))
 
 ;; AUFGABE 2: Dreieckspuzzle
@@ -18,36 +19,49 @@
 ;; Die Implementation geht um die Suche einer bestimmer Subgraph in einem groesseren Ksub9 Graph (fully connected graph mit 9 Knoten). Die Knoten stellen die Teilen ([x y z]) und die Kanten wuerden die moegliche Verbindungen inzwischen 2 Kanten: Maximal koennen 2 Dreicke in 3 verschiedene Weise verbunden werden, weil sie nur 3 Seiten haben und keine zweite Seite kann mit demsselben Dreieck verbunden. Das bedeutet Wir suchen nach 18 besondere Kanten, die durch alle 9 Knoten durchlaufen! In andere Woerter, geht dieses Problem um die komplette Suche eines Graphs. Dafuer kann man schlaue Tricks verwenden um die Suche zu reduzieren, wie zum Beispiel dass wenn es keine Knoten gibt, die mit anderen 3 verbunden werden koennte; dann kann man aus den Teilen keine grosseres Dreieck machen.
 ;; Um das Prozess besser zu erklaeren wird ein vereinfachtes Version des Problem "geloest": 4 kleine Dreiecke (aufgabe2sample2.txt).
 
-Teilen
-
 (defn Superset
   [coll]
   (map #(remove (fn [_] (= _ nil)) %1) (aufgabe1/combinatorics (map (fn [e] [e nil]) coll))))
 
+(logic/defne not-membero
+  [x l]
+  ([_ []]) ;; So, anything is NOT A MEMBER of []... nothing. Includin nothing itself, then follows that [] is contained within _ (bottom)
+  ([_ [?y . ?r]]
+   (logic/!= x ?y) ;; first checks fot the first element, and then continues recursively. If there is at least one element in the collection equal to the one given by us at the beginning of the process... it fails since it is a NESTED IF.
+   (not-membero x ?r)))
+
+(defn paolo
+  [coll1 coll2]
+  ; [[1 2] [2 3]] [[1 2] [3 2]] -> this would evaluate to a wrong answer since [2 3] [3 2] are considered 2 different groups.
+  (logic/fresh [a b] ;; fresh return a function to be used in a RUN BLOCK!!!
+    (logic/conde
+     [(logic/membero a coll1) (not-membero a coll2)]
+     [(logic/membero b coll2) (not-membero b coll1)])))
+
 (defn connects?
   [d1 d2]
-  (logic/run* [q]
-    (logic/membero q (Superset d1))
-    (logic/membero q (Superset d2))))
+  (let [cc (logic/run* [A B]
+             (logic/membero A (Superset d1))
+             (logic/membero B (Superset d2))
+             (logic/nafc paolo A B))]
+    (reduce #(if (> (count %1) (count %2)) %1 %2) (map first cc))))
 
-(reduce #(if (> (count %1) (count %2)) %1 %2) (connects? [1 2 3] [1 1 3]))
+(def karte
+  (letfn [(foo [coll d] (reduce (fn [p n] (assoc p n (connects? d n))) {} coll))]
+    (apply conj (map (fn [T] {T (foo (remove #(= %1 T) Teilen) T)}) Teilen))))
 
-;; This is the criteria for stablishing/creating "edges" in our graph.
-;; {n #{a b c}} means there are n paths between X and Y, each one formed by the union of a, or b, or c.
-(defn beziehung
-  [a b]
-  (let [paths (clojure.set/intersection (set a) (set b))]
-    {[a b] paths}))
+(comment {(2 1 0) {(0 1 0) (1 0),
+                   (2 0 1) (2 1 0),
+                   (0 2 2) (2 0)},
+          (0 1 0) {(2 1 0) (0 1 0),
+                   (2 0 1) (0 1 0),
+                   (0 2 2) (0 0)},
+          (2 0 1) {(2 1 0) (2 0 1),
+                   (0 1 0) (0 1),
+                   (0 2 2) (2 0)},
+          (0 2 2) {(2 1 0) (0 2 2),
+                   (0 1 0) (0),
+                   (2 0 1) (0 2 2)}})
 
-(defn k-graph
-  [nodes]
- (loop [p nodes
-         e []]
-    (if (= (count p) 1)
-      (flatten e)
-      (recur (rest p)
-             (conj e (for [i (rest p)] (beziehung (first p) i)))))))
-
-(k-graph parts)
 
 
