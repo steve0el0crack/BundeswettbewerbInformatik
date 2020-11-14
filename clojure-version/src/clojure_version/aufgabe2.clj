db3a91c606a0ec4a65bbf1c5c22c5af563c28be1
Esteban Castillo <escaob1@gmail.com>
2020-11-14 16:44:16 +0100

    Solved using core.logic

--- clojure-version/src/clojure_version/aufgabe2.clj
+++ clojure-version/src/clojure_version/aufgabe2.clj
@@ -7,97 +7,38 @@
 
 ;; AUFGABE 2: Dreieckspuzzle
 
+(def input
+  (with-open [rdr (clojure.java.io/reader (io/resource "clojure_version/aufgabe2sample.txt"))]
+             (reduce conj [] (line-seq rdr))))
+
+;; Each number was incremented because fd/interval only works with positive numbers. At the end they will be substracted...
+
 (def Teilen
-  (let [file (with-open [rdr (clojure.java.io/reader (io/resource "clojure_version/aufgabe2sample2.txt"))]
-               (reduce conj [] (line-seq rdr)))
-       parts (rest (rest file))] 
+  (let [parts (rest (rest input))] 
     (map (fn [triple]
-           (map #(read-string %1)
+           (map #(+ (read-string %1) (read-string (first input)))  
                 (clojure.string/split triple #" ")))
          parts)))
 
-(defn Superset
-  [coll]
-  (map #(remove (fn [_] (= _ nil)) %1) (aufgabe1/combinatorics (map (fn [e] [e nil]) coll))))
+(logic/run 1 [q]
+  (logic/fresh [n1 n2 n3
+                n4 n5 n6
+                n7 n8 n9]
+    (logic/fresh [c1 b1 a1
+                  c2 b2 a2
+                  c3 b3 a3]
+      (logic/permuteo [c1 b1 a1 c2 b2 a2 c3 b3 a3] Teilen)  ;; this breaks if I am given more pieces than I actually need, or less...
 
-(logic/defne not-membero
-  [x l]
-  ([_ []]) 
-  ([_ [?y . ?r]]
-   (logic/!= x ?y) 
-   (not-membero x ?r)))
+      (logic/permuteo c1 [n1 n2 n3])
+      (logic/permuteo c2 [n4 n5 n6])
+      (logic/permuteo c3 [n7 n8 n9])
 
-(defn paolo
-  [coll1 coll2]
-  (logic/fresh [a b] 
-    (logic/conde
-     [(logic/membero a coll1) (not-membero a coll2)]
-     [(logic/membero b coll2) (not-membero b coll1)])))
+      (macro/symbol-macrolet [_ (logic/lvar)]
+       (logic/permuteo b1 [n1 n4 _])
+       (logic/permuteo b2 [n5 n8 _])
+       (logic/permuteo b3 [n2 n7 _]))
 
-(defn connects?
-  [d1 d2]
-  (let [cc (logic/run* [A B]
-             (logic/membero A (Superset d1))
-             (logic/membero B (Superset d2))
-             (logic/project [A B]
-                            (logic/== (count A) (count B)))
-             (logic/nafc paolo A B))]
-    (reduce #(if (> (count %1) (count %2)) %1 %2) (map first cc))))
-
-
-(def karte
-  (letfn [(map-constructor
-            [coll d]
-            (reduce (fn [p n] (assoc p n (connects? d n)))
-                    {}
-                    coll))]
-    (apply conj (map (fn [T]
-                       {T (map-constructor (remove #(= %1 T) Teilen) T)})
-                     Teilen))))
-(comment
-  {(2 1 0) {(0 1 0) (1 0),
-            (2 0 1) (2 1 0),
-            (0 2 2) (2 0)},
-   (0 1 0) {(2 1 0) (1 0),
-            (2 0 1) (1 0),
-            (0 2 2) (0)},
-   (2 0 1) {(2 1 0) (2 0 1),
-            (0 1 0) (0 1),
-            (0 2 2) (2 0)},
-   (0 2 2) {(2 1 0) (0 2),
-            (0 1 0) (0),
-            (2 0 1) (0 2)}})
-
-(defn get-conjunctions
-  [d]
-  (letfn [(foo [d]
-            (comp
-             (filter (fn [_] (= (first _) d)))
-             (mapcat second)
-             (map second)))]
-    (transduce (foo d) conj [] karte)))
-
-Teilen
-
-((let [center (logic/lvar)
-        bridges (repeatedly 3 logic/lvar)
-        rest (remove #(= center %1) Teilen)]
-    (logic/run* [q]
-      (logic/membero center Teilen)
-      (logic/permuteo bridges center)
-      (logic/fresh [d1 d2 d3]
-        (logic/membero d1 rest)
-        (logic/membero d2 rest)
-        (logic/membero d3 rest)
-        (logic/!= d1 d2)
-        (logic/!= d1 d3)
-        (logic/!= d3 d2)
-        (logic/fresh [a b c]
-          (logic/== [a b c] bridges)
-          (logic/membero a d1)
-          (logic/membero b d2)
-          (logic/membero c d3))
-        (logic/== q [center bridges [d1 d2 d3]])))))
-
-
+      (logic/== q {:c [c1 c2 c3]
+                   :b [b1 b2 b3]
+                   :a [a1 a2 a3]}))))
 
