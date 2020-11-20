d1ba8dc2b3d3ebc153e44e723ef0d5ca0a56d384
Esteban Castillo <escaob1@gmail.com>
2020-11-20 19:12:47 +0100

    Trying to explain the FIRST PART of the configuration/path finding...

--- clojure-version/src/clojure_version/aufgabe5.clj
+++ clojure-version/src/clojure_version/aufgabe5.clj
@@ -43,22 +43,33 @@
 ;;  {vx [...] ... }]               -> :l ast column
 
 ;; Is there any variable with just one possibility? It means if there is a number on any column that appears just once.
-(def fixed-vars (map (fn [h] (filter #(= 1 (count (second %1))) h)) structure))
+(def fixed-vars (map (fn [h] (filter #(= 1 (count (second %1))) h)) structure)) ;; needs more code to get to the pair (key value)
 
-(let [pool (map second (first structure))  ;; I am looking into the first column
-      cond1 (map second (second structure))  ;; then into the second one
-      cond2 (map second (last structure))]  ;; and finally into the last.
-  (logic/run 1 [q]
-    (logic/fresh [a b c x] ;; a, b and c are not going to be the same number since it is impossible for two numbers to occupy the same index on an array...
-      (logic/membero a (nth pool 0))
-      (logic/membero b (nth pool 1))
-      (logic/membero c (nth pool 2))
+(defn hello
+  [parameter] ;; this must be better explained!
+  (let [pool (map second (first structure)) ;; I am looking into the first column
+        cond1 (map second (second structure)) ;; then into the second one
+        cond2 (map second (last structure)) ;; and finally into the last.
+        vars (repeatedly (count pool) logic/lvar)] 
+    (logic/run 1 [q]
 
-      (logic/permuteo x [a b c])
-      (not-membero x cond1)
-      (not-membero x cond2)
+      (rec-membero vars pool)
 
-      (logic/== q [a b c]))))
+      (macro/symbol-macrolet [S (map #(remove (fn [_] (= _ nil)) %1) (Superset [] vars))]
+                             (fd/<= (count (remove #(not-membero S %1) cond1)) parameter)  ;; this is for continuing trying with other values
+                             (fd/<= (count (remove #(not-membero S %1) cond2)) parameter)) ;; " "
+      
+      (logic/== q vars))))
+
+;; this is going to search for the coordinates (indexes) which should be marked in order to NOT BLOCK other unique variables...
+(defn zeta [n]
+  (if (empty? (hello n)) (zeta (+ n 1)) (hello n)))
+
+;; Till now, we have find the following:
+;; * fixed-vars -> Unique variables that must be marked on the first COLUMN, since they are nowhere else (the reasoning behind this procedure is that if they are not marked at THAT COLUMN and at THAT INDEX, then there are two possibilities: It is going to be marked on the one of the 2 next columns, which would give us at the end a WORSE RATING for the selection. The second possibility is that they are not going to be marked at all! That would mean that at the end of the distribution/assignment of gifts; one kid will receive this gift without even having wished for it... it would be completely random)
+;; * hello -> Focusing on those vars which could be positioned at many different indexes, the question is which of these configurations to choose? With the help of a logic engine; one can find the configuration that leaves THE MOST/BETTER ROOM for other variables. The reasoning behind this is that sometimes choosing one of 2 possible coordinates for a determined variable WILL MAKE the difference when choosing the following.
+
+;; In other words, till now we should have the following: "On the first COLUMN the numbers/variables/gifts [...] should be marked at the index's [...]". That means... the first row is complete! And the rest is just repeating the same, taking into account other "parameters"...
 
 (logic/defne rec-membero
   [vars nested-coll]
@@ -67,15 +78,27 @@
    (logic/membero x S)
    (rec-membero xr rS)))
 
-(logic/run 2 [a]
-  (rec-membero [a] [[1 2]]))
+(logic/defne rec-notmembero
+  [l coll]
+  ([_ []])
+  ([_ [?x . ?r]]   
+   (not-membero ?x l)
+   (rec-notmembero l ?r)))
 
 (logic/defne not-membero [x l]
   ([_ []])
   ([_ [?y . ?r]]
-    (logic/!= x ?y)
-    (not-membero x ?r)))
+   (logic/fresh [h]  ;; when working with lists...
+     (logic/permuteo h x)
+     (logic/!= h ?y))
+   (not-membero x ?r)))
 
+(defn Superset
+  [c set]
+  (let [i (gensym)]
+    (if (= (count set) 1)
+      (map (fn [a] (conj c a)) [nil (first set)])
+      (mapcat (fn [i] (Superset (conj c i) (rest set))) [nil (first set)]))))
 
 
 
